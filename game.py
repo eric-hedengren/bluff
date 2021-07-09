0eb914003d53224e5fc39cd6aab8675dc450a665
Eric Hedengren <46898294+eric-hedengren@users.noreply.github.com>
2021-07-09 16:27:52 -0600

    welcome to the jungle

--- game.py
+++ game.py
@@ -1,1 +1,18 @@
 from random import randint
+
+humans = []
+
+while True:
+    human = input("Enter a player name: ").lower()
+
+    if human == '' or human == 'done':
+        break
+    else:
+        humans.append(human)
+
+bots = int(input("Enter the number of bots: "))
+
+if len(humans) + bots < 2:
+    raise Exception("Number of players must be equal or greater to 2. The game wouldn't be fun by yourself!")
+
+players = {}

b01c8d8ab6c0236ac58246d9129b8af35e32ed08
Eric Hedengren <46898294+eric-hedengren@users.noreply.github.com>
2021-07-09 18:42:35 -0600

    player data?

--- game.py
+++ game.py
@@ -10,9 +10,32 @@ while True:
     else:
         humans.append(human)
 
-bots = int(input("Enter the number of bots: "))
+bots = input("Enter the number of bots: ")
+
+if bots == '':
+    bots = 0
+
+bots = int(bots)
 
 if len(humans) + bots < 2:
     raise Exception("Number of players must be equal or greater to 2. The game wouldn't be fun by yourself!")
 
-players = {}
+dice_number = int(input("Enter the number of dice per player: "))
+
+if dice_number <= 0:
+    raise Exception("Number of dice should be greater than 0. Or everyone would lose instantly.")
+
+player_names = humans
+
+for i in range(1,bots+1):
+    player_names.append('bot'+str(i))
+
+player_data = {}
+
+for player_name in player_names:
+    rolled_dice = []
+
+    for i in range(dice_number):
+        rolled_dice.append(randint(1,6))
+
+    player_data[player_name] = rolled_dice

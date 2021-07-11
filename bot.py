2d6b1c4d7e4206e24dc1901d723200e7086faec0
Eric Hedengren <46898294+eric-hedengren@users.noreply.github.com>
2021-07-11 14:30:12 -0600

    sorted rolls! organized functions. no duplicates.

---
+++ bot.py
@@ -0,0 +1,5 @@
+def bot_turn_first(known,unknown):
+    return known,unknown
+
+def bot_turn(guess,known,unknown):
+    return guess,known,unknown
--- game.py
+++ game.py
@@ -1,32 +1,43 @@
+# imports
 import random
+import bot
 
 
+# functions
 def roll(number):
     dice = []
 
     for i in range(number):
         dice.append(random.randint(1,6))
 
-    return dice
+    return sorted(dice)
 
-def validate_guess_first(current):
-    if current[0] <= 0:
-        raise Exception("Not a valid quantity.")
+def known_dice(current_player):
+    revealed_dice = player_data[current_player]['dice']
 
-    if current[1] < 1 or current[1] > 6:
-        raise Exception("You can't guess outside the 1-6 range.")
+    for player_name in player_names:
+        revealed_dice.append(player_data[player_name]['revealed_dice'])
+
+    # make complete list all raw data
+
+    return sorted(revealed_dice)
+
+def unknown_dice(current_player):
+    unknown_dice = 0
+
+    other_players = player_names
+    other_players.remove(current_player)
+
+    for other_player in other_players:
+        unknown_dice += len(player_data[other_player]['dice'])
+
+    return unknown_dice
 
 def validate_guess(current):
-    return None
-
-def bot_turn_first(known,unknown):
-    # guess is the only option
-    return None # return guess
-
-def bot_turn(guess,known,unknown):
-    return None
+    return current
 
 
+# players
 humans = []
 
 while True:
@@ -45,56 +56,76 @@ if bots == '':
 bots = int(bots)
 
 if len(humans) + bots < 2:
-    raise Exception("Number of players must be equal or greater to 2. The game wouldn't be fun with no one or by yourself!")
+    raise Exception("Number of players must be equal or greater to 2. The game can't be played with no one or by yourself!")
 
-dice_number = int(input("Enter the number of dice per player: "))
 
-if dice_number <= 0:
-    raise Exception("Number of dice should be greater than 0. Or everyone would lose instantly.")
-
+# player names
 player_names = humans
 
 for i in range(1,bots+1):
     player_names.append('bot'+str(i))
 
+if len(player_names) != len(set(player_names)):
+    raise Exception("Cannot have duplicate player names. Stats are important and we can't track those if you have duplicates!")
 
-# loop for multiple games
+
+# number of dice
+dice_number = int(input("Enter the number of dice per player: "))
+
+if dice_number <= 0:
+    raise Exception("Number of dice should be greater than 0. Or everyone would lose instantly.")
+
+
+# the main game
 while True:
     # initialize data
+    player_number = len(player_names)
     player_data = {}
 
     for player_name in player_names:
         player_data[player_name] = {'dice': roll(dice_number), 'revealed_dice': []}
 
-    #shuffle player order
+
+    # determine player order
     random.shuffle(player_names)
 
-    player_number = len(player_names)
-    turn = 0
+    turn = 1
+    player_turn = player_names[turn-1]
 
-    player_turn = player_names[turn]
 
-    # first turn
+    # bot first turn
     if player_turn.startswith('bot'):
-        current_guess = bot_turn_first()
+        current_guess = bot.bot_turn_first(known_dice(player_turn),unknown_dice(player_turn))
 
+    # human first turn
     else:
         print("It's {player}'s turn".format(player=player_turn))
-
-        # display their dice, and the number of unknown dice
+        print("These are the dice you have rolled: {dice}".format(dice=player_data[player_turn]['dice']))
+        print("There are {number} dice you cannot see".format(number=unknown_dice(player_turn)))
+        print("What's your guess?")
 
         current_guess = []
         current_guess.append(int(input("How many? ")))
         current_guess.append(int(input("What number? ")))
 
-        validate_guess_first(current_guess)
 
+        if current_guess[0] <= 0:
+            raise Exception("Not a valid quantity.")
+
+        if current_guess[1] < 1 or current_guess[1] > 6:
+            raise Exception("You can't guess outside the 1-6 range.")
+
+
+    # the game after the first turn
     while True:
-        player_turn = player_names[turn%player_number]
+        turn += 1
+        player_turn = player_names[(turn-1)%player_number]
 
+        # bot turn
         if player_turn.startswith('bot'):
-            current_guess = bot_turn()
+            current_guess = bot.bot_turn(current_guess,known_dice(player_turn),unknown_dice(player_turn))
 
+        # human turn
         else:
             print("It's {player}'s turn".format(player=player_turn))
             print("The current guess is {number} {roll}s".format(number=current_guess[0], roll=current_guess[1]))
@@ -103,9 +134,16 @@ while True:
             # let them choose to call a bluff, or take a guess
             # if guess was chosen, let them keep the dice they have, or put some dice out and re roll
 
+        # if a guess was called: 
+        # check who won the round
+        # take away the right amount of dice from certain players
         # check if the game is over
 
         break
+
+
+    # save game stats
+
 
     # end of game
     response = input("Play again? This will use the same settings. ").lower()
